6619bac6da1994c22dfd72c58a358d83adc43359
priya <priya.prasad1994@gmail.com>
2024-08-24 18:18:33 +0530

    First commit

---
+++ src/test/java/TestScript/Test1.java
@@ -0,0 +1,18 @@
+package TestScript;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void Demo()
+	{
+		System.out.println("test 1");
+	}
+	public void Demo1()
+	{
+		System.out.println("test 2");
+	}
+
+
+}
